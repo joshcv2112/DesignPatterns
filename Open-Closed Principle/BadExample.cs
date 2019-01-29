8c2e701d10930a577f88079d134f8ae29e8bfef7
Joshua Vaughan <joshcv2112@gmail.com>
2019-01-29 16:50:28 -0700

    Completed Open-Closed Principle

---
+++ Open-Closed Principle/BadExample.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Closed_Principle
+{
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+
+    public enum Size
+    {
+        Small, Medium, Large
+    }
+
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+
+        public Product(string name, Color color, Size size)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(name));
+            }
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+
+    // This class breaks the open-closed principle.
+    // Open for extension, closed for modification.
+    // We can apply the specification pattern...
+    public class ProductFilter
+    {
+        public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var p in products)
+                if (p.Size == size)
+                    yield return p;
+        }
+
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var p in products)
+                if (p.Color == color)
+                    yield return p;
+        }
+
+        public IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Size size, Color color)
+        {
+            foreach (var p in products)
+                if (p.Size == size && p.Color == color)
+                    yield return p;
+        }
+    }
+
+    class BadExample
+    {
+        /*static void Main(string[] args)
+        {
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+            Product[] products = { apple, tree, house };
+
+            var pf = new ProductFilter();
+            Console.WriteLine("Green products (old): ");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+            {
+                Console.WriteLine($" - {p.Name} is green.");
+            }
+        }*/
+    }
+}
---
+++ Open-Closed Principle/GoodExample.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ The open and closed principle is about being able to extend the
+ functionality of code without having to modify existing classes.
+ If a class/function is already shipped to the customer, you don't
+ want to have to redefine that module when adding new functionality
+ to a project.
+     */
+
+namespace Open_Closed_Principle
+{
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(T t);
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private Color color;
+
+        public ColorSpecification(Color color)
+        {
+            this.color = color;
+        }
+
+        public bool IsSatisfied(Product t)
+        {
+            return t.Color == color;
+        }
+    }
+
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private Size size;
+
+        public SizeSpecification(Size size)
+        {
+            this.size = size;
+        }
+
+        public bool IsSatisfied(Product t)
+        {
+            return t.Size == size;
+        }
+    }
+
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        ISpecification<T> first, second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first ?? throw new ArgumentNullException(paramName: nameof(first));
+            this.second = second ?? throw new ArgumentNullException(paramName: nameof(second));
+        }
+
+        public bool IsSatisfied(T t)
+        {
+            return first.IsSatisfied(t) && second.IsSatisfied(t);
+        }
+    }
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+        {
+            foreach (var i in items)
+                if (spec.IsSatisfied(i))
+                    yield return i;
+        }
+    }
+
+    class GoodExample
+    {
+        static void Main(string[] args)
+        {
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+            Product[] products = { apple, tree, house };
+
+            // Using bad example.
+            var pf = new ProductFilter();
+            Console.WriteLine("Green products (old): ");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+            {
+                Console.WriteLine($" - {p.Name} is green.");
+            }
+
+            // Using good example.
+            var bf = new BetterFilter();
+            Console.WriteLine("Green products (new):");
+            foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+            {
+                Console.WriteLine($" - {p.Name} is green.");
+            }
+
+            // Another good example using color and size in filter.
+            Console.WriteLine("Large blue items:");
+            foreach (var p in bf.Filter(
+                products,
+                new AndSpecification<Product>(
+                    new ColorSpecification(Color.Blue),
+                    new SizeSpecification(Size.Large)
+                )))
+            {
+                Console.WriteLine($" - {p.Name} is big and blue");
+            }
+        }
+    }
+}
